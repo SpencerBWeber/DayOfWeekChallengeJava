9ffee643278d90a1299df195e49a7a0227c7c17b
JingleDjango95 <spencerweber019@gmail.com>
2019-10-23 17:35:02 -0600

    created day of week challenge

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        DayOfWeekChallenge.printDayOfTheWeek(0);
+        DayOfWeekChallenge.printDayOfTheWeek(1);
+        DayOfWeekChallenge.printDayOfTheWeek(2);
+        DayOfWeekChallenge.printDayOfTheWeek(3);
+        DayOfWeekChallenge.printDayOfTheWeek(4);
+        DayOfWeekChallenge.printDayOfTheWeek(5);
+        DayOfWeekChallenge.printDayOfTheWeek(6);
+        DayOfWeekChallenge.printDayOfTheWeek(7);
     }
 }
